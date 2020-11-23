6369e4f2bf35a0e0d1fa72528b3d9c4b9831c4f7
Matt Greer <matt.e.greer@gmail.com>
2020-11-23 15:42:02 -0500

    tailwind: use sans-serif only

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 module.exports = {
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'media', // 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        'accent-1': '#333',
-      },
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+	darkMode: 'media', // 'media' or 'class'
+	theme: {
+		fontFamily: {
+			sans: ['sans-serif'],
+		},
+		extend: {
+			colors: {
+				'accent-1': '#333',
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
