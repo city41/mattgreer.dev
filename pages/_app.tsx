df33cdc312604856b8df40ec6bd0c88ddb0a0263
Matt Greer <matt.e.greer@gmail.com>
2020-11-23 13:09:11 -0500

    prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import '../styles/index.css'
+import React from 'react';
+import '../styles/index.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
-import Nav from '../components/nav'
+import React from 'react';
+import Nav from '../components/nav';
 
 export default function IndexPage() {
-  return (
-    <div>
-      <Nav />
-      <div className="py-20">
-        <h1 className="text-8xl text-center text-gray-700 dark:text-gray-100">
-          Next.js + Tailwind CSS 2.0
-        </h1>
-      </div>
-    </div>
-  )
+	return (
+		<div>
+			<Nav />
+			<div className="py-20">
+				<h1 className="text-8xl text-center text-gray-700 dark:text-gray-100">
+					Next.js + Tailwind CSS 2.0
+				</h1>
+			</div>
+		</div>
+	);
 }
