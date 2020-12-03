3305440d15d246561a391acd8326620be6c04f92
Matt Greer <matt.e.greer@gmail.com>
2020-12-02 20:21:50 -0500

    Article: decent layout

--- components/layouts/Article.tsx
+++ components/layouts/Article.tsx
@@ -8,16 +8,33 @@ type ArticleProps = {
 	children: React.ReactNode;
 };
 
+function toId(s: string) {
+	return s.toLowerCase().replace(/\s/g, '-');
+}
+
 const components = {
-	h2: ({ children }) => <h2 className="text-3xl">{children}</h2>,
-	p: ({ children }) => <p className="my-2 max-w-2xl">{children}</p>,
+	h2: ({ children }) => (
+		<h2 className="text-2xl font-bold mt-32" id={toId(children)}>
+			{children}
+		</h2>
+	),
+	h3: ({ children }) => (
+		<h3 className="font-bold mt-16" id={toId(children)}>
+			{children}
+		</h3>
+	),
+	a: ({ children }) => (
+		<a className="text-focal font-bold hover:underline">{children}</a>
+	),
+	img: (props) => <img className="my-12 max-w-2xl" {...props} />,
+	p: ({ children }) => <p className="my-4 max-w-2xl">{children}</p>,
 	pitfall: ({ children }) => (
 		<div className="my-2 max-w-2xl text-red-900 bg-red-50 p-2 -mx-2">
 			{children}
 		</div>
 	),
 	wisdom: ({ children }) => (
-		<div className="my-2 max-w-2xl text-green-900 bg-green-50 p-2 -mx-2">
+		<div className="my-4 max-w-2xl text-green-900 bg-green-50 p-4 -mx-4">
 			{children}
 		</div>
 	),
@@ -26,9 +43,17 @@ const components = {
 function Article({ title, date, children }: ArticleProps) {
 	return (
 		<MDXProvider components={components}>
-			<div className="max-w-screen-lg lg:mx-auto mx-4">
-				<PageHeading>{title}</PageHeading>
-				<p>written on {new Date(date).toDateString()}</p>
+			<div className="max-w-screen-lg lg:mx-auto mx-4 px-4 sm:px-16 pb-32">
+				<header className="pt-24 mb-16 sm:pt-32 sm:mb-24">
+					<time className="text-xs text-fg-fade" dateTime={date}>
+						{new Date(date).toLocaleDateString('en-us', {
+							year: 'numeric',
+							month: 'short',
+							day: 'numeric',
+						})}
+					</time>
+					<PageHeading>{title}</PageHeading>
+				</header>
 				{children}
 			</div>
 		</MDXProvider>
