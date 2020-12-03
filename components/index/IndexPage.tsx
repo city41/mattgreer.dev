647d8e6ad18397c6fa316a49d80874328f5f12ed
Matt Greer <matt.e.greer@gmail.com>
2020-12-02 20:32:53 -0500

    IndexPaged: remove FadeInOnScroll

--- components/index/IndexPage.tsx
+++ components/index/IndexPage.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Header } from './Header';
 import { Feature } from './Feature';
 import { InternalLink } from '../InternalLink';
-import { FadeInOnScroll } from './FadeInOnScroll';
 
 type IndexPageProps = {
 	items: PortfolioItem[];
@@ -23,20 +22,16 @@ function IndexPage({ items }: IndexPageProps) {
 				<div className="flex flex-col space-y-48 overflow-x-hidden">
 					{items.map((item, index) => {
 						return (
-							<FadeInOnScroll
+							<Feature
 								key={item.slug}
-								comeFrom={index & 1 ? 'right' : 'left'}
-							>
-								<Feature
-									side={index & 1 ? 'left' : 'right'}
-									slug={item.slug}
-									title={item.title}
-									description={item.description}
-									classification={item.classification}
-									type={item.type}
-									tags={item.tags}
-								/>
-							</FadeInOnScroll>
+								side={index & 1 ? 'left' : 'right'}
+								slug={item.slug}
+								title={item.title}
+								description={item.description}
+								classification={item.classification}
+								type={item.type}
+								tags={item.tags}
+							/>
 						);
 					})}
 				</div>
