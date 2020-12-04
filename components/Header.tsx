e4847f1dfc1fb802584ca739a424edea6e42fb15
Matt Greer <matt.e.greer@gmail.com>
2020-12-04 12:18:50 -0500

    PortfolioPage: more whitespace around laptop

--- components/Header.tsx
+++ components/Header.tsx
@@ -21,7 +21,7 @@ function Header({ className, logo, title, page, children }: HeaderProps) {
 				)}
 			>
 				<div
-					className={`header_illustration ${page}_page_header_illustration col-start-2 col-end-8 h-64 sm:h-auto sm:row-span-2 sm:row-start-1 sm:col-start-1 sm:col-end-4 self-stretch`}
+					className={`header_illustration ${page}_page_header_illustration col-start-1 col-end-9 h-64 sm:h-auto sm:row-span-2 sm:row-start-1 sm:col-start-1 sm:col-end-4 self-stretch`}
 				/>
 				<h1 className="col-start-2 col-end-8 sm:row-start-1 sm:col-start-4 sm:col-end-9 text-4xl sm:text-7xl font-black text-focal">
 					{title}
