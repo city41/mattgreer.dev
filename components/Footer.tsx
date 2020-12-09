6317e0e942db7115fbc1f0ad34c3d585b16d0658
Matt Greer <matt.e.greer@gmail.com>
2020-12-09 15:13:16 -0500

    Footer: aria-label for icon links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,10 +14,22 @@ type FooterProps = {
 	className?: string;
 };
 
-function IconLink({ Icon, href }: { Icon: React.ElementType; href: string }) {
+function IconLink({
+	Icon,
+	href,
+	label,
+}: {
+	Icon: React.ElementType;
+	href: string;
+	label: string;
+}) {
 	return (
 		<Link href={href} passHref>
-			<a rel="noopener" className="inline-block p-2 rounded-sm hover:bg-bg">
+			<a
+				rel="noopener"
+				className="inline-block p-2 rounded-sm hover:bg-bg"
+				aria-label={label}
+			>
 				<Icon className="text-2xl sm:text-base" />
 			</a>
 		</Link>
@@ -35,24 +47,38 @@ function Footer({ className }: FooterProps) {
 			<LightDarkToggle />
 			<ul className="mt-1 sm:mt-4 flex flex-row space-x-1">
 				<li>
-					<IconLink Icon={FaEnvelope} href="mailto:matt.e.greer@gmail.com" />
+					<IconLink
+						Icon={FaEnvelope}
+						href="mailto:matt.e.greer@gmail.com"
+						label="email me"
+					/>
 				</li>
 				<li>
-					<IconLink Icon={FaTwitter} href="https://twitter.com/mattegreer" />
+					<IconLink
+						Icon={FaTwitter}
+						href="https://twitter.com/mattegreer"
+						label="my Twitter profile"
+					/>
 				</li>
 				<li>
 					<IconLink
 						Icon={FaLinkedin}
 						href="https://www.linkedin.com/in/matt-greer-133405ab/"
+						label="my LinkedIn profile"
 					/>
 				</li>
 				<li>
-					<IconLink Icon={FaGithub} href="https://github.com/city41" />
+					<IconLink
+						Icon={FaGithub}
+						href="https://github.com/city41"
+						label="my GitHub profile"
+					/>
 				</li>
 				<li>
 					<IconLink
 						Icon={FaStackOverflow}
 						href="https://stackoverflow.com/users/194940/matt-greer"
+						label="my StackOverflow profile"
 					/>
 				</li>
 			</ul>
