43fa9d2d5a9d39c2a846fd91bc810773df7b948b
Matt Greer <matt.e.greer@gmail.com>
2020-12-10 09:30:45 -0500

    starting to work

--- components/LightDarkToggle.tsx
+++ components/LightDarkToggle.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
 import clsx from 'clsx';
 import { FiMoon, FiSun } from 'react-icons/fi';
 
@@ -6,70 +7,93 @@ type LightDarkToggleProps = {
 	className?: string;
 };
 
+const BUTTON_ID = 'LightDarkToggleButton';
 const DARK_CLASS = 'forcedDark';
 const LIGHT_CLASS = 'forcedLight';
 
 function LightDarkToggle({ className }: LightDarkToggleProps) {
-	const [prefersLightMode, setPrefersLightMode] = useState<boolean | null>(
-		null
-	);
-
-	useEffect(() => {
-		const queryList = window.matchMedia('(prefers-color-scheme: light)');
-
-		function handleQueryChange() {
-			// since the user changed their system pref,
-			// remove any manual overrides they may have set earlier
-			document.body.classList.remove(DARK_CLASS, LIGHT_CLASS);
-			setPrefersLightMode(queryList.matches);
-		}
-
-		queryList.addEventListener('change', handleQueryChange);
-
-		// call right away to sync initial state
-		handleQueryChange();
-
-		return () => queryList.removeEventListener('change', handleQueryChange);
-	}, []);
+	// useEffect(() => {
+	// 	const queryList = window.matchMedia('(prefers-color-scheme: light)');
+	//
+	// 	function handleQueryChange() {
+	// 		// since the user changed their system pref,
+	// 		// remove any manual overrides they may have set earlier
+	// 		document.body.classList.remove(DARK_CLASS, LIGHT_CLASS);
+	// 		setPrefersLightMode(queryList.matches);
+	// 	}
+	//
+	// 	queryList.addEventListener('change', handleQueryChange);
+	//
+	// 	// call right away to sync initial state
+	// 	handleQueryChange();
+	//
+	// 	return () => queryList.removeEventListener('change', handleQueryChange);
+	// }, []);
 
 	const handleClick = () => {
 		const body = document.body;
 
 		body.classList.remove(DARK_CLASS, LIGHT_CLASS);
 
-		if (prefersLightMode) {
-			body.classList.add(DARK_CLASS);
-		} else {
-			body.classList.add(LIGHT_CLASS);
-		}
+		const prefersLightMode = localStorage.prefersLightMode === 'true';
 
-		setPrefersLightMode(body.classList.contains(LIGHT_CLASS));
+		if (prefersLightMode) {
+			console.log('going dark');
+			body.classList.add(DARK_CLASS);
+			localStorage.prefersLightMode = 'false';
+		} else {
+			console.log('going light');
+			body.classList.add(LIGHT_CLASS);
+			localStorage.prefersLightMode = 'true';
+		}
 	};
 
-	// if this value never leaves null, then the user has JS disabled,
-	// so in that case no need to render the toggle at all
-	if (prefersLightMode === null) {
-		return null;
-	}
-
 	return (
-		<button
-			className={clsx(className, 'p-2 flex flex-row items-center text-fg-fade')}
-			onClick={handleClick}
-			aria-label="color scheme toggle"
-		>
-			{prefersLightMode ? (
-				<>
+		<>
+			<Head>
+				<script
+					type="text/javascript"
+					dangerouslySetInnerHTML={{
+						__html: `
+DARK_CLASS = '${DARK_CLASS}';
+LIGHT_CLASS = '${LIGHT_CLASS}';
+							
+__queryList = window.matchMedia('(prefers-color-scheme: light)');
+
+if (typeof localStorage.prefersLightMode !== 'string') {
+	localStorage.prefersLightMode = __queryList.matches.toString();
+}
+
+function onDomContentLoaded() {
+	var lightDarkToggleButton = document.getElementById('${BUTTON_ID}');
+	lightDarkToggleButton.addEventListener('click', ${handleClick.toString()});
+	
+	document.removeEventListener('DOMContentLoaded', onDomContentLoaded);
+}
+
+document.addEventListener('DOMContentLoaded', onDomContentLoaded);
+							`,
+					}}
+				></script>
+			</Head>
+			<button
+				id={BUTTON_ID}
+				className={clsx(
+					className,
+					'p-2 flex flex-row items-center text-fg-fade'
+				)}
+				aria-label="color scheme toggle"
+			>
+				<div id="prefersLightModeDisplay">
 					<FiSun className="text-2xl mr-2" aria-label="light mode" />
 					light theme
-				</>
-			) : (
-				<>
+				</div>
+				<div id="prefersDarkModeDisplay">
 					<FiMoon className="text-2xl mr-2" aria-label="dark mode" />
 					dark theme
-				</>
-			)}
-		</button>
+				</div>
+			</button>
+		</>
 	);
 }
 
