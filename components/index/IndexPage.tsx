1135dc88f2707236adcf8cd5cffc5fee1e2c3dc7
Matt Greer <matt.e.greer@gmail.com>
2020-12-28 10:01:49 -0500

    raw new IndexPage in place

--- components/index/IndexPage.tsx
+++ components/index/IndexPage.tsx
@@ -1,43 +1,55 @@
 import React from 'react';
-import { Root } from '../layouts/Root';
-import { Feature } from '../Feature';
+import clsx from 'clsx';
+import styles from '../Header.module.css';
 import { FocalColorLink } from '../FocalColorLink';
 import { ResumeLink } from '../ResumeLink';
+import { Feature } from '../Feature';
+import { Footer } from '../Footer';
 
 type IndexPageProps = {
 	items: PortfolioItem[];
 };
 
 function IndexPage({ items }: IndexPageProps) {
-	const headerContent = (
-		<>
-			<p>
-				Hi! I am a freelance software engineer with a focus on web technologies.
-			</p>
-			<p>
-				Check out my <FocalColorLink href="/projects">projects</FocalColorLink>,{' '}
-				<FocalColorLink href="/articles">articles</FocalColorLink>,{' '}
-				<FocalColorLink href="/about">about page</FocalColorLink>, or my{' '}
-				<ResumeLink />.
-			</p>
-			<p>
-				Or,{' '}
-				<FocalColorLink href="mailto:matt.e.greer@gmail.com">
-					get in touch
-				</FocalColorLink>
-				.
-			</p>
-		</>
-	);
-
 	return (
-		<Root
-			navigation={false}
-			title="Matt Greer"
-			metaDescription="My personal website with things I've made and articles I have written"
-			page="index"
-			headerContent={headerContent}
-		>
+		<div>
+			<div className="h-screen flex flex-col items-center justify-center bg-green-50">
+				<h1 className="text-7xl">Matt Greer</h1>
+				<h2 className="text-2xl">software creator</h2>
+			</div>
+			<div
+				className={clsx(
+					'grid grid-cols-8 gap-8 auto-rows-min max-w-4xl mx-auto px-8 sm:px-0'
+				)}
+			>
+				<div
+					className={clsx(
+						styles.headerIllustration,
+						styles.indexPageHeaderIllustration,
+						'col-start-1 col-end-9 h-64 sm:h-auto sm:row-span-2 sm:row-start-1 sm:col-start-1 sm:col-end-4 self-stretch -mx-8 sm:mx-0'
+					)}
+				/>
+				<div className="col-start-1 col-end-9 sm:row-start-2 sm:col-start-4 sm:col-end-8 sm:text-2xl space-y-8 leading-6 sm:leading-9">
+					<p>
+						Hi! I am a freelance software engineer with a focus on web
+						technologies.
+					</p>
+					<p>
+						Check out my{' '}
+						<FocalColorLink href="/projects">projects</FocalColorLink>,{' '}
+						<FocalColorLink href="/articles">articles</FocalColorLink>,{' '}
+						<FocalColorLink href="/about">about page</FocalColorLink>, or my{' '}
+						<ResumeLink />.
+					</p>
+					<p>
+						Or,{' '}
+						<FocalColorLink href="mailto:matt.e.greer@gmail.com">
+							get in touch
+						</FocalColorLink>
+						.
+					</p>
+				</div>
+			</div>
 			<div className="h-32 grid place-items-center font-bold">
 				Some things I have created{' '}
 			</div>
@@ -53,7 +65,8 @@ function IndexPage({ items }: IndexPageProps) {
 					<FocalColorLink href="/articles">articles</FocalColorLink>
 				</div>
 			</div>
-		</Root>
+			<Footer className="mt-16 sm:mt-24" />
+		</div>
 	);
 }
 
