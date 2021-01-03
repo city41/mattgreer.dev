bf793d9c7135edec14429a0e564331fa92d14113
Matt Greer <matt.e.greer@gmail.com>
2021-01-03 15:24:58 -0500

    Skills: html5 use color scheme

--- components/index/Skills.tsx
+++ components/index/Skills.tsx
@@ -5,27 +5,32 @@ import html5Svg from './html5.svg';
 import tsLogoSvg from './tsLogo.svg';
 import kitchenSinkSvg from './sink.svg';
 
+import styles from './Skills.module.css';
+
 type SkillsProps = {
 	className?: string;
 };
 
 function Skill({
+	imgClassName,
 	img,
 	imgAlt,
 	children,
 }: {
-	img: string;
+	imgClassName?: string;
+	img?: string;
 	imgAlt: string;
 	children: string;
 }) {
-	const imgStyle = { backgroundImage: `url(${img})` };
+	const imgStyle = img ? { backgroundImage: `url(${img})` } : {};
 
 	return (
 		<div className="flex flex-col items-center w-full sm:w-1/3 max-w-6xl px-0 sm:px-8 mt-8 sm:mt-0">
 			<div
-				className={clsx('w-40 h-40 bg-cover bg-no-repeat bg-center', {
-					'bg-green-500': !img,
-				})}
+				className={clsx(
+					imgClassName,
+					'w-40 h-40 bg-cover bg-no-repeat bg-center'
+				)}
 				style={imgStyle}
 				role="img"
 				aria-label={imgAlt}
@@ -39,7 +44,7 @@ function Skills({ className }: SkillsProps) {
 	return (
 		<div className={className}>
 			<div className="flex flex-col sm:flex-row justify-between items-start mx-8 sm:mx-16">
-				<Skill img={html5Svg} imgAlt="HTML5 logo">
+				<Skill imgClassName={styles.html5} imgAlt="HTML5 logo">
 					Deep understanding of HTML, CSS and HTTP including the many features
 					of HTML5 and CSS3.
 				</Skill>
