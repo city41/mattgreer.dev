814b0e610527ec7e14070e8b3a8bf4c80fe2f178
Matt Greer <matt.e.greer@gmail.com>
2021-01-06 10:10:18 -0500

    Feature: dont nudge external link icon

--- components/Feature.tsx
+++ components/Feature.tsx
@@ -62,7 +62,7 @@ function Feature({
 						<h2 className="text-4xl sm:text-5xl font-bold mt-8 sm:mt-0 xtext-center sm:text-left hover:text-focal-alt">
 							{title}
 							{external && (
-								<HiExternalLink className="inline text-2xl text-fg-fade -mb-2" />
+								<HiExternalLink className="inline text-2xl text-fg-fade" />
 							)}
 						</h2>
 					</a>
