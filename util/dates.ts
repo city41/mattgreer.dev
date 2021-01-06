0cb17f0b56f5dc4f16adfdb8173bfc56374bae90
Matt Greer <matt.e.greer@gmail.com>
2021-01-06 17:38:11 -0500

    dates util: simulate en-gb dates

--- util/dates.ts
+++ util/dates.ts
@@ -1,11 +1,25 @@
+/**
+ * Note that Date#toLocaleDateString() cannot be used in Node easily.
+ * The Node install needs to install the full ICU set, and getting that going
+ * on vercel for example is probably more trouble than its worth. So this method
+ * is handcoded a bit. Only need one date format, so not a huge deal
+ */
 function dateToHumanString(input: Date | number | string): string {
 	const date = new Date(input);
 
-	return date.toLocaleDateString('en-gb', {
-		year: 'numeric',
+	const month = date.toLocaleDateString('en-us', {
 		month: 'short',
+	});
+
+	const day = date.toLocaleDateString('en-us', {
 		day: 'numeric',
 	});
+
+	const year = date.toLocaleDateString('en-us', {
+		year: 'numeric',
+	});
+
+	return `${day} ${month} ${year}`;
 }
 
 export { dateToHumanString };
