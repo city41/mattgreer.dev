6de84cb0904ea80273fdf07fd1dc084e758f0861
Matt Greer <matt.e.greer@gmail.com>
2021-01-26 15:43:45 -0500

    submit with urlencoded data

--- components/blog/posts/using-google-drive-for-form-submissions/DemoForm.tsx
+++ components/blog/posts/using-google-drive-for-form-submissions/DemoForm.tsx
@@ -2,49 +2,56 @@ import React, { useState } from 'react';
 
 const POST_URL =
 	'https://docs.google.com/forms/u/0/d/e/1FAIpQLSegO8Qy4C6KrXa8x3wd7tW05xvDq6Qrv4_PNJOld_vIE3ipXA/formResponse';
+const entryName = 'entry.249370976';
 
 function DemoForm() {
-	const [textEditor, setTextEditor] = useState<string | null>(null);
+	const [formData, setFormData] = useState({});
 	const [hasSubmitted, setHasSubmitted] = useState(false);
 
 	async function handleSubmit(e: React.FormEvent) {
 		e.stopPropagation();
 		e.preventDefault();
 
-		const body = JSON.stringify({
-			'entry.249370976': textEditor,
+		const encodedValues = Object.keys(formData).map((key) => {
+			return `${encodeURIComponent(key)}=${encodeURIComponent(formData[key])}`;
 		});
 
-		await fetch(POST_URL, {
-			method: 'POST',
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-			},
-			body,
-		});
-
-		setHasSubmitted(true);
+		try {
+			await fetch(POST_URL, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+				},
+				body: encodedValues.join('&').replace(/%20/g, '+'),
+			});
+		} catch (e) {
+		} finally {
+			setHasSubmitted(true);
+		}
 	}
 
 	if (hasSubmitted) {
-		return <div>Thanks, I like ${textEditor} too!</div>;
+		return <div>Thanks, I like {formData[entryName]} too!</div>;
 	} else {
 		return (
 			<form method="post" onSubmit={handleSubmit} action={POST_URL}>
 				<select
-					name="entry.249370976"
-					onChange={(e) => setTextEditor(e.target.value)}
-					value={textEditor}
+					name={entryName}
+					onChange={(e) => {
+						e.persist();
+						setFormData((f) => ({ ...f, [entryName]: e.target.value }));
+					}}
+					value={formData[entryName]}
 				>
-					<option value="atom">Atom</option>
-					<option value="emacs">Emacs</option>
-					<option value="sublime">Sublime</option>
-					<option value="textmate">Textmate</option>
+					<option>-</option>
+					<option value="Atom">Atom</option>
+					<option value="Emacs">Emacs</option>
+					<option value="Sublime">Sublime</option>
+					<option value="Textmate">Textmate</option>
 					<option value="vim">vim</option>
 					<option value="VSCode">VSCode</option>
 				</select>
-				<input type="submit" value="submit" />
+				<input type="submit" value="submit" disabled={!formData[entryName]} />
 			</form>
 		);
 	}
