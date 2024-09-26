29da92d7e25c8b148414cb6de7243587c59aab43
Matt Greer <matt.e.greer@gmail.com>
2024-09-25 20:24:49 -0400

    ok i think all the math is correct

--- src/components/balatro/BalatroMathJax.tsx
+++ src/components/balatro/BalatroMathJax.tsx
@@ -12,19 +12,20 @@ function BalatroMathJax({ latex }: BalatroMathJaxProps) {
 		setDomLoaded(true);
 	}, []);
 
+	if (!domLoaded) {
+		return <></>;
+	}
+
 	return (
 		<>
-			{domLoaded && Array.isArray(latex) ? (
+			{Array.isArray(latex) ? (
 				<div className="text-xl w-full flex flex-col gap-y-2 my-16 border border-fg-fade py-4 px-8 text-fg-deep">
 					{latex.map((l) => {
 						return <Tex2SVG display="block" latex={l} />;
 					})}
 				</div>
 			) : (
-				<Tex2SVG
-					class="inline-block text-xl text-fg-deep"
-					latex={latex as string}
-				/>
+				<Tex2SVG class="inline-block" display="block" latex={latex} />
 			)}
 		</>
 	);
